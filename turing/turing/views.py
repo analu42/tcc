6b098860b7c96718f525901710208d8d92aac051
analu <130846375+analu42@users.noreply.github.com>
2024-12-24 22:09:50 -0300

    Initial commit

---
+++ turing/turing/views.py
@@ -0,0 +1,10 @@
+# from django.http import HttpResponse
+from django.shortcuts import render
+
+def homepage(request):
+    # return HttpResponse('O Jogo da Imitação')
+    return render(request, 'index.html')
+
+def about(request):
+    # return HttpResponse("My About Page.")
+    return render(request, 'about.html')
